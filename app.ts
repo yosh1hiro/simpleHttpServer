fb7f45089eba99952fba21d085773abcc9920c11
yoshihiro asada <yoshihiro0921@gmail.com>
2016-07-15 16:38:13 +0900

    Modify app.ts why divided standing server function into serverModule

--- app.ts
+++ app.ts
@@ -1,18 +1,9 @@
-/// <reference path="./node.d.ts" />;
+import serverModule = require('./serverModule');
 
-import http = require('http');
-
-/**
- * Main
- */
 class Main {
   constructor() {
-    let server = http.createServer((request:http.ServerRequest, response:http.ServerResponse) => this.requestHandler(request, response));
-    server.listen("5000");
-  }
-
-  private requestHandler(request:http.ServerRequest, response:http.ServerResponse):void {
-    response.end("Hello! Node.js with TypeScript!");
+    let serverAPI: serverModule.ServerAPI = new serverModule.ServerAPI();
+    serverAPI.initServer();
   }
 }
-var main: Main = new Main();
+let main: Main = new Main();
